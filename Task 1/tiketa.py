1b201f9cf8f0c2805b157e73fc8199b764e40100
Dz Sk <dzmitry.skarabahatau@gmail.com>
2021-04-05 19:23:34 +0300

    Clean up and comments

--- Task 1/tiketa.py
+++ Task 1/tiketa.py
@@ -1,3 +1,4 @@
+from constants import *
 from locators import Locators
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,27 +6,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-TIMEOUT_s = 3
-URL = 'https://www.tiketa.lt/EN/search'
-ARENA_NAME = 'Avia Solutions Group arena'
-EVENT_NAME = 'CIRQUE DU SOLEIL - CORTEO'
-QUERY = 'Corteo'
-
-
 d = webdriver.Chrome()
 wait = WebDriverWait(d, TIMEOUT_s)
 d.get(URL)
 d.find_element(*Locators.caption_field).send_keys(QUERY + Keys.TAB)
+
+# needs to be clicked for the arena options to become enabled
 d.find_element(*Locators.event_place_dropdown).click()
 d.find_element(*Locators.arena(ARENA_NAME)).click()
-d.find_element(*Locators.date_from).send_keys('2022-01-01')
-d.find_element(*Locators.date_to).send_keys('2022-03-31')
+
+d.find_element(*Locators.date_from).send_keys(DATE_FROM)
+d.find_element(*Locators.date_to).send_keys(DATE_TO)
 d.find_element(*Locators.search).click()
-event_item = wait.until(EC.element_to_be_clickable(Locators.event_item(EVENT_NAME)))
-buy_buttons = d.find_elements(*Locators.buy_button)
+
+# because of element hierarchy, there's no way to directly map from event name to buy button element
+# so, we construct a list of img elements look at their alt property
+# the index that matches the event name is the index of the buy button of interest
 img_elements = d.find_elements(*Locators.img)
-buy_button = buy_buttons[[element.get_attribute('alt') for element in img_elements].index(EVENT_NAME)]
+buy_buttons = wait.until(EC.visibility_of_all_elements_located(Locators.buy_button))
+event_index = [element.get_attribute('alt') for element in img_elements].index(EVENT_NAME)
+buy_button = buy_buttons[event_index]
+
+# the slow scrolling animation on the results pages causes to our clicks to become intercepted by the cookies banner
+# one workaround for this behaviour is to click using js
+# another option would be to write our own EC class with a try/except block
+# or just explicitly wait a number of seconds
 d.execute_script('arguments[0].click()', buy_button)
 options = wait.until(EC.visibility_of_all_elements_located(Locators.options))
+
 print(f'Final item count: {len(options)}')
 d.close()
