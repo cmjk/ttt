ae247f92e5cce64379e41b8def0b542296db15c0
Dz Sk <dzmitry.skarabahatau@gmail.com>
2021-04-06 19:48:00 +0300

    task 3 works

---
+++ Task 3/challenging_dom.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from time import sleep
+
+URL = 'http://the-internet.herokuapp.com/challenging_dom'
+GREEN_BUTTON_LOCATOR = 'a.button.success'
+
+
+def highlight_element(element, duration):
+    script = "arguments[0].setAttribute('style', arguments[1]);"
+    driver = element._parent
+    original_style = element.get_attribute('style')
+    driver.execute_script(script, element, 'border: 3px solid red;')
+    sleep(duration)
+    driver.execute_script(script, element, original_style)
+
+
+def highlight_row_of_column(driver: webdriver.Chrome, column_name: str, row_index: int, duration_s: int = 2) -> None:
+    header = driver.find_elements_by_css_selector('thead th')
+    column_index = [item.text for item in header].index(column_name)
+    cell_locator = f'tr:nth-child({row_index}) td:nth-child({column_index + 1})'
+    cell_element = d.find_element_by_css_selector(cell_locator)
+    highlight_element(cell_element, duration_s)
+
+
+def highlight_button_from_text(driver: webdriver.Chrome, text_value: str, button: str, duration_s: int =2 ) -> None:
+    locator = f'//td[text() = "{text_value}"]/..//a[@href="#{button}"]'
+    element = driver.find_element_by_xpath(locator)
+    highlight_element(element, duration_s)
+
+
+def highlight_cell_from_text(driver: webdriver.Chrome, text_value: str, duration_s: int = 2) -> None:
+    locator = f'//td[text() = "{text_value}"]'
+    element = driver.find_element_by_xpath(locator)
+    highlight_element(element, duration_s)
+
+
+d = webdriver.Chrome()
+d.get(URL)
+
+highlight_row_of_column(d, 'Diceret', 3)
+highlight_button_from_text(d, 'Apeirian7', 'delete')
+highlight_button_from_text(d, 'Apeirian2', 'edit')
+highlight_cell_from_text(d, 'Definiebas7')
+highlight_cell_from_text(d, 'Iuvaret7')
+d.find_element_by_css_selector(GREEN_BUTTON_LOCATOR).click()
+d.close()
